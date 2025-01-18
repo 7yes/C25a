41542a242051d61321768b1f9551eb300befb432
yesmc <7yesid@gmail.com>
2025-01-18 17:26:27 -0600

    ajustes

--- app/src/main/java/com/jesse/c25a/paging3tutorial/data/response/ResponseWrapper.kt
+++ app/src/main/java/com/jesse/c25a/paging3tutorial/data/response/ResponseWrapper.kt
@@ -1,8 +1,6 @@
 package com.jesse.c25a.paging3tutorial.data.response
 
 import com.google.gson.annotations.SerializedName
-import com.jesse.c25a.paging3tutorial.data.response.CharacterResponse
-import com.jesse.c25a.paging3tutorial.data.response.InfoResponse
 
 data class ResponseWrapper(
     @SerializedName("info") val information: InfoResponse,
